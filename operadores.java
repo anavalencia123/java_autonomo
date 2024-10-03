73b28cdf0ba957a72c998186c4c2f4fa05a19274
anavalencia123 <martinezlauramartinez2006@gmail.com>
2024-10-03 12:50:04 -0500

    operadores

---
+++ operadores.java
@@ -0,0 +1,38 @@
+public class operadores {
+    public static void main(String[] args) {
+        int a=5;
+        int b;
+        b=7;
+        int c=b+a;
+        c-=6;
+        System.out.println("la suma es:" +(c));
+        int d=c*a;
+        System.out.println("la multiplicacion es: " +(d));
+        int divicion=d/c;
+        System.out.println("la divicon es:" +(divicion));
+        int resta=divicion-a;
+        System.out.println("la resta es:"+(resta));
+        final double cons=10;
+        double cm=6;
+        double resultado=cm/cons;
+        System.out.println("el resultado es:"+(resultado));
+        int e=8,f=9,g=3,u=45;
+        System.out.println("es"+(e) +"es"+(f));
+
+    }
+}
+//public class operadores 
+//public static void main(String[] args) {
+        //double a=5;
+        //double b;
+        //b=7;
+        //double c=b+a;
+        //c-=6;
+        //System.out.println("la suma es:" +(c));
+        //double d=c*a;
+        //System.out.println("la multiplicacion es: " +(d));
+        //double divicion=d/c;
+        //System.out.println("la divicon es:" +(divicion));
+        //double resta =divicion-a;
+        //System.out.println("la resta es :"+(resta));
+    
