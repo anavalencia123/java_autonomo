661137da573c6b7e543bfd608a5ffb02e4d6b745
anavalencia123 <martinezlauramartinez2006@gmail.com>
2024-10-02 23:12:01 -0500

    cambios

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte edad = 34 ;
+// optimisar recursos 
+        edad = 45;
+        System.out.println(edad);
+    }
+}
+// poo 
--- primerospasos/hola_mundo.java
+++ primerospasos/hola_mundo.java
@@ -2,4 +2,5 @@ public class hola_mundo{
     public static void main(String[] args) {
         System.out.println("hola mundo");
     }
+    
 }
