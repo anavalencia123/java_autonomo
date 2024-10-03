6ea501e09fed40d432bdb7567eda94995d9d5cf9
anavalencia123 <martinezlauramartinez2006@gmail.com>
2024-10-03 14:24:31 -0500

    math

--- primerospasos/calse_math.java
+++ primerospasos/calse_math.java
@@ -3,6 +3,13 @@ public class calse_math {
     
         double raiz = Math.sqrt(25);
         System.out.println(raiz);
+        double num1=5.85;
+        int resultado=(int)Math.round(num1);
+        System.out.println(resultado);
+        double base=5;
+        double exponenete =3;
+        int resultado2=(int)Math.pow(base,exponenete);
+        System.out.println(resultado2);
 
     }
 }
